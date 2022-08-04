c0d9cd29604d05d7f23450a682f6f17df1f2d633
almounica <108088289+almounica@users.noreply.github.com>
2022-08-04 10:30:11 +0530

    Add files via upload

---
+++ Servlet/src/main/java/com/example/demo/User.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+public class User {
+
+	private int uid;
+	private String name;
+	private String address;
+	
+	
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getname() {
+		return name;
+	}
+	public void setname(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address =address;
+	}
+
+}
