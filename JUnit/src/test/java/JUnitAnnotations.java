e9456e87180e05a905f64df9db76deab008a87e4
almounica <108088289+almounica@users.noreply.github.com>
2022-08-04 10:37:42 +0530

    Add files via upload

---
+++ JUnit/src/test/java/JUnitAnnotations.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.*;
+
+public class JUnitAnnotations {
+    @BeforeAll
+    public static void beforeAll()
+    {
+        System.out.println("Executed once before all test methods");
+    }
+    @BeforeEach
+    public void beforeEach()
+    {
+        System.out.println("Execute before each test methods");
+    }
+    @Test
+    public void testMethod1()
+    {
+        System.out.println("Execute test method1");
+    }
+    @Test
+    @Disabled("implementation pending")
+    public void testMethod3()
+    {
+        System.out.println("Execute test method3");
+    }
+
+    @DisplayName("TestMethod2")
+    @Test
+    public void abc()
+    {
+        System.out.println("Execute test TestMethod2");
+    }
+    @AfterAll
+    public static void afterAll()
+    {
+        System.out.println("Executed once after all test methods");
+    }
+    @AfterEach
+    public void afterEach()
+    {
+        System.out.println("Execute after each test methods");
+    }
+}
+
+
---
+++ JUnit/src/test/java/JUnitAssumptions.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class JUnitAssumptions {
+    @Test
+    void testAssumeTrue(){
+        boolean b = 'a'=='a';
+        assumeTrue(b);
+        assertEquals("Hello","Hello");
+
+    }
+
+    @Test
+    @DisplayName("Test Execute only Friday")
+    public void testassumeTrue(){
+        LocalDateTime dt = LocalDateTime.now();
+
+        assumeTrue(dt.getDayOfWeek().getValue()==5);
+        System.out.println("Further code will execute only if above assumption hold true");
+    }
+    @Test
+    void testassumeFalse(){
+        boolean b = 'a' != 'a';
+        assumeFalse(b);
+        assertEquals("Hello", "Hello");
+    }
+
+    @Test
+    void testassumeflaseEnvProp(){
+        System.setProperty("env","Prod");
+        assumeFalse("dev".equals((System.getProperty("env"))));
+        System.out.println("Further code will execute only if above assumption hold");
+    }
+}
